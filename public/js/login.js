25652048a44f87cee81d6c20feec6fcf85247154
Archit Bhonsle <abhonsle2000@gmail.com>
2020-02-29 12:55:57 +0530

    Basic login working

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+let googleProvider = new firebase.auth.GoogleAuthProvider();
+
+document.getElementById("google-login").onclick = firebase
+    .auth()
+    .signInWithPopup(googleProvider)
+    .then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        console.log(token);
+        console.log(user);
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
