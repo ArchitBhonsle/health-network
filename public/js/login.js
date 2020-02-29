ee6f147cbb59fd12d94aeb518d440d24e18740c9
Archit Bhonsle <abhonsle2000@gmail.com>
2020-02-29 13:40:34 +0530

    "Beautiful" login page 1.0

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,26 @@
 let googleProvider = new firebase.auth.GoogleAuthProvider();
 
-document.getElementById("google-login").onclick = firebase
-    .auth()
-    .signInWithPopup(googleProvider)
-    .then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        console.log(token);
-        console.log(user);
-    })
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
+document.getElementById("google-login").addEventListener("click", () => {
+    firebase
+        .auth()
+        .signInWithPopup(googleProvider)
+        .then(function(result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+            console.log(token);
+            console.log(user);
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+});
