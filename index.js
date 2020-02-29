7eb040e4278ac6d362a85fd2a79bf2657a44d1aa
Archit Bhonsle <abhonsle2000@gmail.com>
2020-02-29 10:28:35 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const firebase = require("firebase");
+
+const firebaseConfig = {
+    apiKey            : "AIzaSyAs8FVXBJ-M4toU6-GNQKDuAsodbkP501w",
+    authDomain        : "social-network-c462e.firebaseapp.com",
+    databaseURL       : "https://social-network-c462e.firebaseio.com",
+    projectId         : "social-network-c462e",
+    storageBucket     : "social-network-c462e.appspot.com",
+    messagingSenderId : "132403966735",
+    appId             : "1:132403966735:web:a5104971fbbb768ce7847a",
+    measurementId     : "G-FD1QD59BLT"
+};
+
+firebase.initializeApp(firebaseConfig);
