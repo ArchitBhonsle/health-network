d7a60309b074fd009f3c114c63ec13d5782c1212
Archit Bhonsle <abhonsle2000@gmail.com>
2020-02-29 14:43:27 +0530

    Reorganization

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,9 @@
 let googleProvider = new firebase.auth.GoogleAuthProvider();
-let facebookProvider = new firebase.auth.FacebookAuthProvider();
 
 document.getElementById("google-login").addEventListener("click", function() {
     firebase
         .auth()
         .signInWithPopup(googleProvider)
-        .then(function(result) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            // ...
-            console.log(token);
-            console.log(user);
-        })
-        .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
-});
-
-document.getElementById("facebook-login").addEventListener("click", function() {
-    firebase
-        .auth()
-        .signInWithPopup(facebookProvider)
         .then(function(result) {
             // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
